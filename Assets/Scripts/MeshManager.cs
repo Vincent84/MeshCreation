1b73c2eba327d13d367882fe9f7b1505fa4a5b4e
Vincenzo Villani <v.villani84@alice.it>
2017-11-29 14:48:28 +0100

    Updated creation

--- Assets/Scripts/MeshManager.cs
+++ Assets/Scripts/MeshManager.cs
@@ -28,38 +28,55 @@ public class MeshManager : MonoBehaviour {
 
         meshRenderer.material.color = Color.black;
 
+        StartCoroutine(MeshCreator());
+
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            mesh.Clear();
 
-            int randomVertice = Random.Range(-30, 30);
-            //int randomVertice = 9;
-            newVertices = new Vector3[] { new Vector3(0, 0, 0),
-                                          new Vector3(0, randomVertice, 0),
-                                          new Vector3(randomVertice, randomVertice, 0),
-                                          new Vector3(0, 0, 0),
-                                          new Vector3(0, randomVertice, 0),
-                                          new Vector3(randomVertice, randomVertice, 0)
-            };
-            newUV = new Vector2[] { new Vector2(0, 0),
-                                    new Vector2(0, randomVertice),
-                                    new Vector2(randomVertice, randomVertice),
-                                    new Vector2(0, 0),
-                                    new Vector2(0, randomVertice),
-                                    new Vector2(randomVertice, randomVertice)
-            };
-            mesh.vertices = newVertices;
-            mesh.uv = newUV;
-            mesh.triangles = newTriangles;
 
-            GetComponent<MeshRenderer>().material.color = Color.black;
-
+        
             //mesh.triangles = newTriangles;
-        }
+        
+    }
+
+    IEnumerator MeshCreator()
+    {
+        
+        mesh.Clear();
+
+        int randomY1 = Random.Range(1, 51);
+        int randomX1 = Random.Range(1, 51);
+
+        int randomY2 = Random.Range(1, 51);
+        int randomX2 = Random.Range(1, 51);
+
+        //int randomVertice = 9;
+        newVertices = new Vector3[] { new Vector3(0, 0, 0),
+                                          new Vector3(0, randomY1, 0),
+                                          new Vector3(randomX1, randomY1, 0),
+                                          new Vector3(0, 0, 0),
+                                          new Vector3(0, -(Mathf.Abs(randomY2)), 0),
+                                          new Vector3(-(Mathf.Abs(randomX2)), -(Mathf.Abs(randomY2)), 0)
+            };
+        newUV = new Vector2[] { new Vector2(0, 0),
+                                    new Vector2(0,  randomY1),
+                                    new Vector2(randomX1, randomY1),
+                                    new Vector2(0, 0),
+                                    new Vector2(0,  -(Mathf.Abs(randomY2))),
+                                    new Vector2(-(Mathf.Abs(randomX2)), -(Mathf.Abs(randomY2)))
+            };
+
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
+
+        GetComponent<MeshRenderer>().material.color = Color.black;
+
+        yield return new WaitForSeconds(2f);
+
+        StartCoroutine(MeshCreator());
     }
 
 }
